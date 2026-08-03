6d5c301a831b3809bbb6fb173b441a0c77722a0f
Caleb Boerma <calebb2202@gmail.com>
2026-08-03 14:08:12 -0400

    mobile working with a few sizing issues

--- src/js/start.js
+++ src/js/start.js
@@ -1,31 +1,57 @@
 // VARIABLES:
-const startBtn = document.querySelector(".start-btn")
+const topBubbles = document.querySelector(".top-bubbles");
+const bottomBubbles = document.querySelector(".bottom-bubbles");
+const startBtn = document.querySelector(".start-btn");
+const logo = document.querySelector(".logo");
 // animal elements
 const animals = document.querySelectorAll(".animal");
 
-let animalJump = 0; // variable tracking if user is hovering "GET STARTED" button
+// used to make jump animation only play on hover (desktop) or play without hovering (mobile)
+let mobile = false;
 
+window.addEventListener("resize", screenChanges);
+
+function screenChanges() {
+    if (window.innerWidth <= 768) {
+        mobile = true;
+        topBubbles.src = "./src/assets/Mobile Asset/SVG/SVG Quiz Cover/QuizCoverUpperBubbleMobile.svg";
+        bottomBubbles.src = "./src/assets/Mobile Asset/SVG/SVG Quiz Cover/QuizCoverLowerBubblesMobile.svg";
+        logo.src = "src/assets/Mobile Asset/SVG/SVG Quiz Cover/QuizCoverMoblieLogo.svg";
+    } else {
+        mobile = false;
+        topBubbles.src = "./src/assets/Desktop Asset/SVG/SVG Quiz Cover/QuizCoverUpperBubbleDesktop.svg";
+        bottomBubbles.src = "./src/assets/Desktop Asset/SVG/SVG Quiz Cover/QuizCoverLowerBubblesDesktop.svg";
+        logo.src = "./src/assets/Desktop Asset/SVG/SVG Quiz Cover/QuizCoverDeskTopLogo.svg";
+
+    }
+}
+screenChanges()
+
+
+// function tracks user hovering start-btn, when hovering the animals jump.
+//  when the user stops hovering it makes sure to not instantly pause the animation
+//  instead it awaits for jump to finish then pauses the animation cycle
 function setAnimalJump (animal) {
-    let userHovering = false;
-    // user hovering
-    startBtn.addEventListener("mouseover", () => {
-        userHovering = true;
+    if (mobile) {
         animal.style.animationPlayState = "running";
-    });
-    // user stop hovering
-    startBtn.addEventListener("mouseout", () => { 
-        userHovering = false;
-    });
+    } else {
+        let userHovering = false;
+        // user hovering
+        startBtn.addEventListener("mouseover", () => {
+            userHovering = true;
+            animal.style.animationPlayState = "running";
+        });
+        // user stop hovering
+        startBtn.addEventListener("mouseout", () => {
+            userHovering = false;
+        });
 
-    // when animation end check if user still hovering if not stop animation
-    animal.addEventListener("animationiteration", () => {
-        if (!userHovering) {
-            animal.style.animationPlayState = "paused";
-        }
-    });
+        // when animation end check if user still hovering if not stop animation
+        animal.addEventListener("animationiteration", () => {
+            if (!userHovering) {
+                animal.style.animationPlayState = "paused";
+            }
+        });
+    }
 }
 animals.forEach(setAnimalJump);
-
-async function sleep(ms) {
-  await new Promise((resolve) => setTimeout(resolve, ms));
-}
